6efc627022847eb4a5910a3d3194e36053b2d58a
Jagat Prajapati <jagatprajapati@lftechnoloty.comn>
2013-08-16 13:00:21 +0545

    added support

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -8,6 +8,6 @@ class SupportsController < ApplicationController
 	end
 	def create
 		SupportMailer.notify(params[:support]).deliver
-		
+		redirect_to new_support_path, notice: 'We will contact you asap.'
 	end
 end
--- app/models/support.rb
+++ app/models/support.rb
@@ -1,4 +1,7 @@
 class Support
 	include ActiveModel::Model
 	attr_accessor :subject, :message
+
+	validates_presence_of :subject
+	#validates :subject, message: true
 end
