4ed849ffa13602cedb64c9d9328199267babf781
Jagat Prajapati <jagatprajapati@lftechnoloty.comn>
2013-08-21 11:10:12 +0545

    added unique title to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,16 @@
 class Album < ActiveRecord::Base
 	belongs_to :user
 	has_many :photos
-	validates :title, presence: true
+	validates :title, presence: true, uniqueness: true
+	#validates_uniqueness_of :title
+
+	validates :user_id, presence: true
+
+	before_save :uppercase_title
+
+	#before_create :aaa
+
+	def uppercase_title
+		self.title = self.title.capitalize
+	end
 end
